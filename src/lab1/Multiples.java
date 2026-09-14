2393cd0c65ae1a18be020690a2e317f576a7c085
punk-but-dead <cheshirsh@gmail.com>
2026-09-14 16:47:58 -0400

    counted multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,26 @@
 package lab1;
 
 public class Multiples {
+    public static void main(String[] args) {
+        countMultiples();
+    }
+
+    private static void countMultiples() {
+        int threemult = 0;
+        int fivemult = 0;
+        int i = 1;
+        while (i < 1000){
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3){
+                threemult++;
+            } else if (divisibleBy5){
+                fivemult++;
+            }
+            i++;
+        }
+        int total = threemult + fivemult;
+        System.out.println(total);
+    }
 }
